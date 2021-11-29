200a53d256e1aeb58615e4292b5d14750085e533
pengjin <pengjin@vcredit.com>
2021-11-29 18:45:18 +0800

    feat: add Grid

--- packages/rc-ui-lib/src/index.ts
+++ packages/rc-ui-lib/src/index.ts
@@ -5,5 +5,6 @@ export { default as Divider } from './divider';
 export { default as Icon } from './icon';
 export { default as Loading } from './loading';
 export { default as Tag } from './tag';
+export { default as Grid } from './grid';
 export { default as ConfigProvider } from './config-provider';
 export * from './hooks';
