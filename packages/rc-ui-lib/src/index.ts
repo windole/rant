ad9e084e2d970febc36d860e987b65ead32104ed
pengjin <pengjin@vcredit.com>
2021-11-30 10:25:36 +0800

    feat: add flex component

--- packages/rc-ui-lib/src/index.ts
+++ packages/rc-ui-lib/src/index.ts
@@ -2,6 +2,7 @@ export { default as Button, ButtonGroup } from './button';
 export { default as Badge } from './badge';
 export { default as Cell } from './cell';
 export { default as Divider } from './divider';
+export { default as Flex } from './flex';
 export { default as Icon } from './icon';
 export { default as Loading } from './loading';
 export { default as Tag } from './tag';
